002c3751818df967bf31381693a90d4b839b2359
Nibha Rajesh Belle <107346911+Nibhabelle18@users.noreply.github.com>
2025-09-11 14:25:47 +0530

    added service layer

--- loginservice/src/main/java/com/example/repository/LoginRepository.java
+++ loginservice/src/main/java/com/example/repository/LoginRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoginRepository extends JpaRepository<UserEntity, String> {
+public interface LoginRepository extends JpaRepository<UserEntity, Long> {
 }
---
+++ loginservice/src/main/java/com/example/service/LoginService.java
@@ -0,0 +1,49 @@
+package com.example.service;
+
+import com.example.entity.UserEntity;
+import com.example.repository.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LoginService {
+    @Autowired
+    private LoginRepository loginRepository;
+
+    public Optional<UserEntity> findById(Long id) {
+        return loginRepository.findById(id);
+    }
+
+    public List<UserEntity> findAllUsers() {
+        return loginRepository.findAll();
+    }
+
+    public void registerUser(UserEntity user) {
+        loginRepository.save(user);
+    }
+
+    public void updateUser(Long id, UserEntity updatedUser) {
+        Optional<UserEntity> existingUser = loginRepository.findById(id);
+        if (existingUser.isPresent()) {
+            UserEntity user = existingUser.get();
+            if (updatedUser.getPassword() != null && !updatedUser.getPassword().isBlank()) {
+                user.setPassword(updatedUser.getPassword());
+            }
+
+            loginRepository.save(user);
+        }
+    }
+
+    public void deleteUser(Long id) {
+        loginRepository.deleteById(id);
+    }
+
+    public boolean validateCredentials(Long id, String password) {
+        Optional<UserEntity> userOpt = loginRepository.findById(id);
+        if (userOpt.isPresent()) {
+            return userOpt.get().getPassword().equals(password);
+        }
+        return false;
+    }
+}
